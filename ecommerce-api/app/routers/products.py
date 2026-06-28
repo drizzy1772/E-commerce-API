066a012b3b448ba6cc291318fe65930d23c04436
drizzy1772 <bodyafrozen890@gmail.com>
2026-06-29 00:51:35 +0300

    completed all tasks that was marked

--- ecommerce-api/app/routers/products.py
+++ ecommerce-api/app/routers/products.py
@@ -8,7 +8,9 @@ from app.database import get_db
 from app.schemas.schemas import ProductResponse
 from app.services.product_service import get_products
 from fastapi_cache.decorator import cache
+import logging
 
+logger = logging.getLogger(__name__)
 
 router = APIRouter(prefix="/products", tags=["products"])
 
@@ -23,7 +25,7 @@ def read_items(
     limit: int = 20,
     db: Session = Depends(get_db)
 ):
-    print("Invite was approved")
+    logger.info(f"Fetching products = {search}, category_id={category_id}, min_price={min_price}, max_price={max_price}")
     return get_products(db, search, category_id, min_price, max_price, skip, limit)
 
 
