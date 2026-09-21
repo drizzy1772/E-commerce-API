42401c4b22cb594fe864885770c76d2bf7c1502c
drizzy1772 <bodyafrozen890@gmail.com>
2026-09-21 22:18:01 +0000

    orderview added

--- frontend/src/views/OrdersView.ts
+++ frontend/src/views/OrdersView.ts
@@ -76,103 +76,40 @@ function render(container: HTMLElement, state: OrdersViewState) {
     if (state.loading === true) {
         container.innerHTML = "<h2>Orders loading...</h2>";
         return;
-    };
+    }
 
     if (state.error !== null) {
         container.innerHTML = `<h2 style="color: red;">Error: ${state.error}</h2>`
         return;
     }
     
-    if (state.loading === false) {
-        container.innerHTML = "<h2>Stats loaded!</h2>";
-    };
-}
-    
-
-
-
-
-
-
-
-////
-
-
-
-interface Product {
-    id: string | number,
-    title: string,
-    price: number,
-    inStock: boolean;
-}
-
-interface ProductsViewState {
-    products: Product[],
-    loading: boolean,
-    error: string | null
-}
-
-export function ProductsView(
-    container: HTMLElement,
-    httpClient: HTTPClient
-): () => void {
-
-    
-
-    const state : ProductsViewState = {
-        products: [],
-        loading: true,
-        error: null
-    };
-
-    const updateUI = () => render(container, state);
-
-    updateUI();
-
-    fetchProducts(httpClient, state, updateUI);
-}
-
-async function fetchProducts(
-        httpClient: HTTPClient,
-        state: ProductsViewState,
-        updateUI: () => void
-    ) {
-        try{
-            const data = await httpClient.request<Product[]>("/api/v1/products");
-            state.products = data;
-            state.loading = false;
-            updateUI();
-        } catch(error: any) {
-            state.error = error.message;
-            state.loading = false;
-            updateUI();
-        }
+   
+    if (state.orders.length === 0) {
+    container.innerHTML = "<h2>You didnt have orders right now</h2>"
+    return;
     }
-    function render(
-        container: HTMLElement,
-        state: ProductsViewState
-    ) {
-           if (state.loading === true) {
-        container.innerHTML = "<h2>Products loading...</h2>";
-        return;
-    };
 
-    if (state.error !== null) {
-        container.innerHTML = `<h2 style="color: red;">Error: ${state.error}</h2>`
-        return;
-    }
-    
-    if (state.loading === false) {
-        const productsHTML = state.products.map(product => {
-            return `
-                <div>
-                    <h3>${product.title}</h3>
-                    <p>Price: ${product.price} UAH</p>
-                    <p>${product.inStock ? "In stock" : "Out of stock"}</p>
-                </div>
-            `;
+    const tableRows = state.orders.map(order => {
+        return `<tr>
+            <td style="border: 1px solid #ccc; padding: 8px;">${order.id}</td>
+               <td style="border: 1px solid #ccc; padding: 8px;">${order.status}</td>
+               <td style="border: 1px solid #ccc; padding: 8px;">$${order.total}</td>
+           </tr>`;
         }).join("");
-        container.innerHTML = productsHTML
-    };
-}
 
+    container.innerHTML = `
+        <h2>Your orders</h2>
+        <table style="width: 100%; border-collapse: collapse; text-align: left;">
+               <thead style="background: #f4f4f4;">
+                   <tr>
+                       <th style="border: 1px solid #ccc; padding: 8px;">ID</th>
+                       <th style="border: 1px solid #ccc; padding: 8px;">Stats</th>
+                       <th style="border: 1px solid #ccc; padding: 8px;">Total</th>
+                   </tr>
+               </thead>
+               <tbody>
+                   ${tableRows}
+               </tbody>
+           </table>
+       `;
+    }
